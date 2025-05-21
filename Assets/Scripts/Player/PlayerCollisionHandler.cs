55e37b8397927d8e20d8ea9cdb3c02035d998f3b
Isaiah Sherman <shermancg@gmail.com>
2025-05-20 20:51:27 -0700

    Added stumble animation
    Stumble animation from Mixamo
    Hook up to Animator, animation transitions, parameters, trigger parameter via code, hit cooldown

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -1,9 +1,41 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    [SerializeField] Animator playerAnimator;
+    [SerializeField] float gotHitCooldown = 1f;
+    [SerializeField] float initialInvulnPeriod = 2f;
+    const string gotHitString = "gotHit";
+    bool canBeHit = false; // Start as false to prevent hits at game start
+
+    void Start()
+    {
+        StartCoroutine(InitialInvulnCo());
+    }
+
+    // This coroutine allows the player to be invulnerable for a short time at the start
+    IEnumerator InitialInvulnCo()
+    {
+        yield return new WaitForSeconds(initialInvulnPeriod);
+        canBeHit = true;
+    }
+
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision detected with: " + collision.gameObject.name);        
+        if (canBeHit)
+        {
+            playerAnimator.SetTrigger(gotHitString);
+            Debug.Log("Collision detected with: " + collision.gameObject.name);
+            StartCoroutine(GotHitCooldownCo());
+        }
     }
+
+    IEnumerator GotHitCooldownCo()
+    {
+        canBeHit = false;
+        yield return new WaitForSeconds(gotHitCooldown);
+        canBeHit = true;
+    }
+
 }
